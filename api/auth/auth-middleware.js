019ad6fcae6157b999c5a5b2738e75094cfe578c
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-31 09:55:06 +0200

    started

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.method('/register', async (req, res, next) => {
+     
+})
+router.method('/register', async (req, res, next) => {
+     
+})
+router.method('/register', async (req, res, next) => {
+     
+})
+
+module.exports = router
